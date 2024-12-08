32d18ca28ec079e0c47453832e744890682a174d
unknown <sujoydaspc2023@gmail.com>
2024-12-08 12:21:51 +0600

    nav responsive

--- src/componenet/Header/Header.jsx
+++ src/componenet/Header/Header.jsx
@@ -1,11 +1,19 @@
 
+import { useState } from 'react'
 import search from '../../../public/imgs/Frame (1).png'
 import profile from '../../../public/imgs/Frame.png'
 const Header = () => {
-    const toggleHandler = () =>{
-        console.log('Clicked');
-        const toggle = document.getElementById('nav-for-sm-dv')
-        toggle.className = 'flex'
+    const [navforsm, setNavforsm] = useState(false)
+    const toggle = document.getElementById('nav-for-sm-dv')
+    const toggleHandler = () => {
+        if (navforsm) {
+            toggle.classList.remove('hidden')
+            toggle.classList.add('flex')
+        } else {
+            toggle.classList.remove('flex')
+            toggle.classList.add('hidden')
+        }
+
     }
     return (
         <header className="border p-10">
@@ -22,12 +30,12 @@ const Header = () => {
                         <img className="lg:block hidden w-5 h-5 ml-3 mr-2" src={search} alt="" />
                         <input className="bg-transparent lg:block hidden outline-none  font-semibold" type="text" placeholder="Search" />
                     </div>
-                    <button onClick={()=>{toggleHandler()}}>
+                    <button onClick={() => { toggleHandler() }}>
                         <img className="bg-[#0BE58A] rounded-full p-2 active:scale-95 hover:bg-emerald-400 w-10 h-10" src={profile} alt="" />
                     </button>
                 </div>
 
-                <div id='nav-for-sm-dv' className="flex flex-col gap-2 absolute right-0 top-12 border  bg-white p-5 rounded-md text-xl">
+                <div id='nav-for-sm-dv' className="hidden flex-col gap-2 absolute right-0 top-12 border  bg-white p-5 rounded-md text-xl">
                     <a href="#" className="hover:text-[#0BE58A] font-semibold border-2-transparent hover:border-b-[#068f56] hover:border-b-2 transition-all active:scale-95">Home</a>
                     <a href="#" className="hover:text-[#0BE58A] font-semibold border-2-transparent hover:border-b-[#068f56] hover:border-b-2 transition-all active:scale-95">Recipes</a>
                     <a href="#" className="hover:text-[#0BE58A] font-semibold border-2-transparent hover:border-b-[#068f56] hover:border-b-2 transition-all active:scale-95">About</a>
